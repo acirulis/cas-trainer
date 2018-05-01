3af3ec67116ecf579571471fd1c6743e23597aaa
Andis Cirulis <andis.cirulis@whitedigital.eu>
2018-05-01 13:43:41 +0300

    sloth config

---
+++ sloth_config.py
@@ -0,0 +1,89 @@
+LABELS = (
+    {"attributes": {"type":  "rect",
+                    "class": "card_2",
+                   },
+     "item":     "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text":     "Card 2"
+    },
+
+    {"attributes": {"type":  "rect",
+                    "class": "card_3",
+                   },
+     "item":     "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text":     "Card 3"
+    },
+
+    {"attributes": {"type":  "rect",
+                    "class": "card_4",
+                   },
+     "item":     "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text":     "Card 4"
+    },
+    {"attributes": {"type": "rect",
+                    "class": "card_5",
+                    },
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text": "Card 5"
+     },
+    {"attributes": {"type": "rect",
+                    "class": "card_6",
+                    },
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text": "Card 6"
+     },
+    {"attributes": {"type": "rect",
+                    "class": "card_7",
+                    },
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text": "Card 7"
+     },
+    {"attributes": {"type": "rect",
+                    "class": "card_8",
+                    },
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text": "Card 8"
+     },
+    {"attributes": {"type": "rect",
+                    "class": "card_9",
+                    },
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text": "Card 9"
+     },
+    {"attributes": {"type": "rect",
+                    "class": "card_J",
+                    },
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text": "Card J"
+     },
+    {"attributes": {"type": "rect",
+                    "class": "card_Q",
+                    },
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text": "Card Q"
+     },
+    {"attributes": {"type": "rect",
+                    "class": "card_K",
+                    },
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text": "Card K"
+     },
+    {"attributes": {"type": "rect",
+                    "class": "card_A",
+                    },
+     "item": "sloth.items.RectItem",
+     "inserter": "sloth.items.RectItemInserter",
+     "text": "Card A"
+     }
+)
+
